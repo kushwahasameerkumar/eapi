b18247043e052706876f69752f2dd690742b9868
Sameer Kumar Kushwaha <kushwahasameerkumar@gmail.com>
2019-07-08 19:53:11 +0530

    Created Model -a

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\MOdels\product;
+use Faker\Generator as Faker;
+
+$factory->define(product::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Models\review;
+use Faker\Generator as Faker;
+
+$factory->define(review::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
