e46790d2084799868f371aeab8c3f8ab6b35ec57
Sameer Kumar Kushwaha <kushwahasameerkumar@gmail.com>
2019-07-08 23:23:37 +0530

    Seeded DB

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,12 +1,18 @@
 <?php
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
-
+use App\Models\product;
 use App\Models\review;
 use Faker\Generator as Faker;
 
-$factory->define(review::class, function (Faker $faker) {
+$factory->define(App\Models\Review::class, function (Faker $faker) {
     return [
         //
+        'product_id' => function(){
+            return product::all()->random();
+        },
+        'star' => $faker->numberBetween(1,5),
+        'customer' => $faker->name,
+        'review' => $faker->paragraph
     ];
 });
